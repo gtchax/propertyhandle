c96b479704bef04ca85065a33d66eed684c10f59
Gwinyai Chakonda <gtchax@gmail.com>
2022-05-26 09:33:47 +0200

    updated chakra-ui

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 import { Box } from "@chakra-ui/layout";
 import Navbar from "./Navbar";
-import SideBar from "./SideBar";
+import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" left="0">
-        <SideBar />
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px" bg="#F9F9F9" minH="100vh">
         <Navbar />
--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 
-// const signedinPages = ["/dashboard/admin", "/dashboard/issuer", '/dashboard/investor'];
-const signedinPages = ['/df'];
+const signedinPages = ["/dashboard/admin", "/dashboard/issuer", '/dashboard/investor'];
+// const signedinPages = ['/df'];
 
 export default function middleware(NextRequest) {
   const url = NextRequest.nextUrl.clone();
